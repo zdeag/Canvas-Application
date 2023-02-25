3824c0521b77328b38acb41884a3c43abf001f3c
Zachary De Aguiar <deaguiarzachary@gmail.com>
2023-02-24 19:19:04 -0500

    Updated Course Service and Student Service

---
+++ Class.Library/Canvas.Models/Services/CourseService.cs
@@ -0,0 +1,54 @@
+﻿using Class.Library.Canvas.Models.Courses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class.Library.Canvas.Models.Services
+{
+    public class CourseService
+    {
+        private List<Course> CourseList;
+        private static CourseService? _instance;
+
+        public static CourseService Current
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new CourseService();
+                }
+
+                return _instance;
+            }
+        }
+
+        private CourseService() 
+        {
+            CourseList = new List<Course>();
+        }
+
+        public void AddCourse(Course course)
+        {
+            CourseList.Add(course);
+        }
+
+        public List<Course> Courses
+        {
+            get
+            {
+                return CourseList;
+            }
+        }
+
+        public IEnumerable<Course> Search(string query)
+        {
+            return Courses.Where(s => s.Name.ToUpper().Contains(query.ToUpper())
+                || s.Description.ToUpper().Contains(query.ToUpper())
+                || s.Code.ToUpper().Contains(query.ToUpper()));
+        }
+
+    }
+}
---
+++ Class.Library/Canvas.Models/Services/StudentService.cs
@@ -0,0 +1,53 @@
+﻿using Class.Library.Canvas.Models.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class.Library.Canvas.Models.Services
+{
+    public class StudentService
+    {
+        private List<Person> studentList;
+
+        private static StudentService? _instance;
+
+        private StudentService() 
+        {
+            studentList = new List<Person>();
+        }
+
+        public static StudentService Current
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new StudentService();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void AddStudent(Person student)
+        {
+            studentList.Add(student);
+        }
+
+        public List<Person> Students
+        {
+            get
+            {
+                return studentList;
+            }
+        }
+
+        public IEnumerable<Person> Search(string query)
+        {
+            return studentList.Where(s => s.Name.ToUpper().Contains(query.ToUpper()));
+        }
+
+    }
+}
